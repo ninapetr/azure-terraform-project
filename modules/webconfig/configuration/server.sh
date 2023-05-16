d328f4e144b7bc08bd643a8210a9b28af9711bda
Yanina Petrova <yaninapetrova63@gmail.com>
2023-05-16 15:48:41 +0200

    test

--- modules/webconfig/configuration/server.sh
+++ modules/webconfig/configuration/server.sh
@@ -3,4 +3,11 @@ sudo apt-get update
 sudo apt-get install -y apache2 git
 
 sudo systemctl restart apache2
+
 sudo git clone https://github.com/Azure-Samples/msdocs-flask-postgresql-sample-app.git
+# sudo /usr/bin/python3 -m venv env
+# add env variables to it
+
+# install requirements
+
+# run the app
